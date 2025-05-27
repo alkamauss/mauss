a1d9c06a4217e56c239f983778e7f9a8e003b85a
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-05-27 16:03:31 +0700

    fix relative entry path

--- workspace/mauss/scripts/size.ts
+++ workspace/mauss/scripts/size.ts
@@ -4,7 +4,7 @@ import pkg from '../package.json';
 module.exports = Object.keys(pkg.exports)
 	.filter((p) => !p.slice(1).includes('.'))
 	.map((entry) => ({
-		path: `src/${entry.slice(2) || 'core'}/index.ts`,
+		path: `../src/${entry.slice(2) || 'core'}/index.ts`,
 		name: `mauss/${entry === '.' ? 'core' : entry.slice(2)}`,
 		import: '*',
 	})) satisfies SizeLimitConfig;
