13e7adea493f86a7f0df3cfa51ad1bb484a46bf7
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-12 19:34:04 +0700

    breaking: prefer async `debounce`

--- workspace/mauss/src/core/processor/index.ts
+++ workspace/mauss/src/core/processor/index.ts
@@ -3,27 +3,6 @@ import type { AnyFunction } from '../../typings/helpers.js';
 const DURATION = 300;
 
 /**
- * Prevent execution of `fn` until `time` has passed
- *
- * @example
- *
- * ```js
- * function update(name) {...} // can also be inlined
- *
- * const search = debounce(update, 500);
- *
- * search('mauss'); // execute after 500ms
- * ```
- */
-export function debounce<F extends AnyFunction>(fn: F, time = DURATION) {
-	let timeout: NodeJS.Timeout;
-	return <A extends Parameters<F>>(...args: A) => {
-		if (timeout) clearTimeout(timeout);
-		timeout = setTimeout(() => fn(...args), time);
-	};
-}
-
-/**
  * Immediately execute `fn` and prevent the next execution until after `time`
  *
  * @example
