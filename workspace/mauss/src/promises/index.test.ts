aa0f73349ae62e2536590f1746209abb9822297c
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-16 12:22:35 +0700

    simplify

--- workspace/mauss/src/promises/index.test.ts
+++ workspace/mauss/src/promises/index.test.ts
@@ -3,19 +3,21 @@ import { debounce } from './index.js';
 
 declare function expect<T>(v: T): void;
 
-const str = (_: string) => {};
-const num = (_: number) => {};
-const name = (_: { name: string }) => {};
+(/* debounce */) => {
+	const str = (_: string) => {};
+	const num = (_: number) => {};
+	const name = (_: { name: string }) => {};
 
-expect<UnaryFunction<string>>(debounce(str));
-expect<UnaryFunction<number>>(debounce(num));
-expect<UnaryFunction<{ name: string }>>(debounce(name));
+	expect<UnaryFunction<string>>(debounce(str));
+	expect<UnaryFunction<number>>(debounce(num));
+	expect<UnaryFunction<{ name: string }>>(debounce(name));
 
-// ---- errors ----
+	// ---- errors ----
 
-// @ts-expect-error
-expect<UnaryFunction<number>>(debounce(str));
-// @ts-expect-error
-expect<UnaryFunction<string>>(debounce(num));
-// @ts-expect-error
-expect<UnaryFunction<{ name: boolean }>>(debounce(name));
+	// @ts-expect-error
+	expect<UnaryFunction<number>>(debounce(str));
+	// @ts-expect-error
+	expect<UnaryFunction<string>>(debounce(num));
+	// @ts-expect-error
+	expect<UnaryFunction<{ name: boolean }>>(debounce(name));
+};
