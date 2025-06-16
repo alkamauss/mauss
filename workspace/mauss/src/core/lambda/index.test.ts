cc2ebbe5608e40a83b7ee48b47fdc343d8da341f
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-16 12:24:04 +0700

    chore: fix types testing (#280)

--- workspace/mauss/src/core/lambda/index.test.ts
+++ workspace/mauss/src/core/lambda/index.test.ts
@@ -1,18 +1,6 @@
-import { attempt, curry, pipe } from './index.js';
+import { curry, pipe } from './index.js';
 
 declare function expect<T>(v: T): void;
-
-async (/* attempt */) => {
-	type Result<T> = { data?: T; error?: unknown };
-
-	expect<Promise<Result<void>>>(attempt(async () => {}));
-	expect<Result<void>>(await attempt(async () => {}));
-	expect<Result<void>>(attempt.sync(() => {}));
-
-	expect<Promise<Result<string>>>(attempt(async () => ''));
-	expect<Result<string>>(await attempt(async () => ''));
-	expect<Result<string>>(attempt.sync(() => ''));
-};
 
 (/* curry */) => {
 	const sum = (a: number, b: number, c: number) => a + b + c;
