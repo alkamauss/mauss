bd186571d998e8265575dcba77599b9e7029f2a8
Ignatius Bagus <ignatius.mbs@gmail.com>
2025-06-18 11:03:16 +0700

    breaking: remove `/bits` and `/math`

---
+++ workspace/mauss/src/core/array/reduce.ts
@@ -0,0 +1,50 @@
+// aggregations and scans
+
+/**
+ * Calculates the mean from a list of numbers using an incremental approach (moving average algorithm), which uses a constant space and updates in constant time. This method helps avoid potential numerical instability issues when dealing with a large sum, such as an integer overflow.
+ *
+ * @example
+ * ```javascript
+ * // returns 3
+ * average([1, 2, 3, 4, 5]);
+ * ```
+ */
+export function average(numbers: number[]): number {
+	let mean = 0;
+	for (let i = 0; i < numbers.length; i++) {
+		mean += (numbers[i] - mean) / (i + 1);
+	}
+	return mean;
+}
+
+export function count<T>(array: T[], predicate?: (item: T) => boolean): number {
+	if (!predicate) return array.length;
+
+	let count = 0;
+	for (let i = 0; i < array.length; i++) {
+		if (predicate(array[i])) count++;
+	}
+	return count;
+}
+
+/**
+ * Find the minimum and maximum values in an array of numbers
+ */
+export function minmax(array: number[]): [min: number, max: number] {
+	if (!array.length) return [0, 0];
+
+	let min = array[0], max = array[0]; // prettier-ignore
+	for (let i = 1; i < array.length; i++) {
+		min = array[i] < min ? array[i] : min;
+		max = array[i] > max ? array[i] : max;
+	}
+	return [min, max];
+}
+
+export function sum(array: number[]): number {
+	let total = 0;
+	for (let i = 0; i < array.length; i++) {
+		total += array[i];
+	}
+	return total;
+}
--- workspace/mauss/src/core/index.ts
+++ workspace/mauss/src/core/index.ts
@@ -3,10 +3,14 @@ export function scope<T>(fn: () => T) {
 	return fn();
 }
 
+export { partition } from './array/group.js';
+export { average, count, minmax, sum } from './array/reduce.js';
+export { binary } from './array/search.js';
 export { attempt } from './attempt/index.js';
 export { date } from './date/index.js';
 export { curry, pipe } from './lambda/index.js';
-export { immediate, throttle } from './processor/index.js';
+export { clamp, modulo } from './number/index.js';
 export { random } from './random/index.js';
 export { capitalize, identical, indent, inverse, regexp, sides, unique } from './standard/index.js';
 export { catenate, tsf } from './string/index.js';
+export { immediate, throttle } from './timing/index.js';
